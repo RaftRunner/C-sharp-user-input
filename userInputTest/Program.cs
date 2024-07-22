76d3609d8ba4a87b18b3776b300f9565e75931c1
Tyler <95229871+RaftRunner@users.noreply.github.com>
2024-07-22 21:39:22 +0100

    Add files via upload

---
+++ userInputTest/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program{
+    static void Main(string[] args){
+
+    //Username example for strings
+    Console.WriteLine("Enter username: ");
+    string userName = Console.ReadLine();
+    Console.WriteLine("Username is: " + userName);
+
+    //Age example for ints
+    Console.WriteLine("Enter your age:");
+    int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Your age is: " + age);
+
+    Console.WriteLine("So you're " + userName + " and you're " + age);
+    }
+}
